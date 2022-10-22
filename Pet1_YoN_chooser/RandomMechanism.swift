2c66848d3639091229704cd6e52db6a558d6f102
deidg <64797316+deidg@users.noreply.github.com>
2022-10-22 19:14:50 +0300

    commit 9

--- Pet1_YoN_chooser/RandomMechanism.swift
+++ Pet1_YoN_chooser/RandomMechanism.swift
@@ -9,12 +9,12 @@ import Foundation
 
 
 
-func randomMechanism() {
-    let names: [String] = ["Yes", "No"]
-
-    //    let names: [String] = ["12", "24", "56", "37", "32", "87"]
-    var randomResult = names.randomElement()!
-    print(randomResult)
-    print("randomMachanism  сработал")
-}
+//func randomMechanism() {
+//    let names: [String] = ["Yes", "No"]
+//
+//    //    let names: [String] = ["12", "24", "56", "37", "32", "87"]
+//    let randomResult = names.randomElement()!
+//    print(randomResult)
+//    print("randomMachanism  сработал")
+//}
 // посмотреть в примерах как возвращать
--- Pet1_YoN_chooser/ViewController.swift
+++ Pet1_YoN_chooser/ViewController.swift
@@ -10,143 +10,95 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
-    
     @IBOutlet weak var label: UILabel!
-    
     @IBOutlet weak var imageFace: UIImageView!
-    
     @IBOutlet weak var imageTail: UIImageView!
-    
     
     @IBAction func yesButton(_ sender: UIButton) {
         
         randomMechanism()
         image.isHidden = false
-        
-        
-        //        label.text = "\(String(describing: randomMechanism))"
-        //        label.text = "YES button pressed"
     }
     
     @IBAction func noButton(_ sender: UIButton) {
-        randomMechanism()
         
+        randomMechanism()
         image.isHidden = false
-
-//        label.text = "\(String(describing: randomMechanism))"
-//        label.text = "NO button pressed"
-//        image.isHidden = false
-//        image.image = UIImage(named: "2euro_Spain_250_reverse")
-
     }
-//      randomMechanism     randomResult
-    
-//    if randomMechanism = "Yes" ? []
-   
-//
-    
-//    
-////
-//    let image = UIImageView() {
-//        image.image = UIImage(named: "2euro_250_avers")
-////            let image = UIImageView(frame: .zero)
-//        self.image.image = UIImage(named: "2euro_Spain_250_reverse")
-//            image.contentMode = .scaleToFill
-//            image.translatesAutoresizingMaskIntoConstraints = false
-//            return image
-//        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        imageFace.image = UIImage(named: "2euro_250_avers")    ok var
-//        imageFace.backgroundColor = UIColor(red: 188/255, green: 227/255, blue: 214/255, alpha: 1)
-//        imageTail.image = UIImage(named: "2euro_Spain_250_reverse")  ok var
-//        imageTail.backgroundColor = UIColor(red: 188/255, green: 227/255, blue: 214/255, alpha: 1)
-        imageFace.layer.cornerRadius = 50
-        imageTail.layer.cornerRadius = 50
+        
+//        yesButton.configuration.cornerStyle{ 5 }
+//            noButton.configuration.cornerStyle{ 10 }
+
+//        layer.cornerRadius = 10
+//        noButton.layer.cornerRadius = 50
+
+
+//        imageFace.layer.cornerRadius = 50
+//        imageTail.layer.cornerRadius = 50
         textLabel()
-        image.image = UIImage(named: "2euro_250_avers")
+//        image.image = UIImage(named: "2euro_250_avers copy-modified")
         image.isHidden = true
-//        image.image = UIImage(named: "2euro_Spain_250_reverse")
-
-        
-        
-        //label.isHidden = false
-        //
-//        if yesButton. {
-//        case 0:
-//            label.text = "The first segment is selected"
-//            label.textColor = .red
-//        case 1:
-//            label.text = "The second segment is selected"
-//            label.textColor = .blue
-//        case 2:
-//            label.text = "The third segment is selected"
-//            label.textColor = .yellow
-//        default:
-//            print("Smth wrong!")
-//        }
-        
-        
-        
-        
-        
-        
-//        image.image = UIImage(named: "2euro_250_avers")
-//        image.image = UIImage(named: "2euro_Spain_250_reverse")
-//        image.backgroundColor = UIColor(red: 188/255, green: 227/255, blue: 214/255, alpha: 1)
-
-
-        //        changeImage()
     }
     
     func textLabel() {
         label.text = "Привет! Бросай монетку!"
-//        label.backgroundColor = UIColor(red: 10/255, green: 10/255, blue: 10/255, alpha: 1)
-//
-    }
-//
-//    func changeImage() {
-//        let imageNew = UIImage()
-//        let imageView = UIImage("1euro_avers")
-//                let image2 = UIImage(named: imageView)
-        //
-        //
-        //    }
-        //    let imageMain = UIImage(named: "animated_teaser")
-        
-        //    func buttonPressed(_ sender: UIButton) {
-        
-        //
+        //        "Привет! Бросай монетку! \r\n  Да      или      Нет"
     }
     
-    // QZs$wB?kDnw8
+    func randomMechanism() {
+        let names: [String] = ["Yes", "No"]
+        
+        //    let names: [String] = ["12", "24", "56", "37", "32", "87"]
+        let randomResult = names.randomElement()!
+        
+        if randomResult == "Yes" {
+            image.image = UIImage(named: "2euro_250_avers copy-modified")
+            label.text = "YES!"
+//            image.layer.cornerRadius = 50
+//            image.backgroundColor = UIColor(red: 188/255, green: 227/255, blue: 214/255, alpha: 1)
+            
+//            проверка
+            print(randomResult)
+               print("randomMachanism  сработал")
+        } else {
+            image.image = UIImage(named: "2euro_Spain_250_reverse copy-modified")
+            label.text = "NO!"
+//            image.layer.cornerRadius = 50
+//            image.backgroundColor = UIColor(red: 188/255, green: 227/255, blue: 214/255, alpha: 1)
+            //            проверка
+            print(randomResult)
+               print("randomMachanism  сработал")
+        }
+        
+        
+        
+        // QZs$wB?kDnw8
+        
+        
+        //extension.  code from ya.ru
+        
+        
+        
+        
+    }
+    // выбор варианта картинки
     
-    
-    //extension.  code from ya.ru
-f
-    
-if randomMechanism = "Yes" {
-    image.image = UIImage(named: "2euro_250_avers")
-} else {
-        image.image = UIImage(named: "2euro_Spain_250_reverse")
+    //label.isHidden = false
+    //
+    //switch segmentedControl.selectedSegmentIndex {
+    //case 0:
+    //    label.text = "The first segment is selected"
+    //    label.textColor = .red
+    //case 1:
+    //    label.text = "The second segment is selected"
+    //    label.textColor = .blue
+    //case 2:
+    //    label.text = "The third segment is selected"
+    //    label.textColor = .yellow
+    //default:
+    //    print("Smth wrong!")
+    //}
 }
-
-
-// выбор варианта картинки
-
-//label.isHidden = false
-//
-//switch segmentedControl.selectedSegmentIndex {
-//case 0:
-//    label.text = "The first segment is selected"
-//    label.textColor = .red
-//case 1:
-//    label.text = "The second segment is selected"
-//    label.textColor = .blue
-//case 2:
-//    label.text = "The third segment is selected"
-//    label.textColor = .yellow
-//default:
-//    print("Smth wrong!")
-//}
